f7c95369d1293a7c50e3cc35c550c5ec04f0bed2
kboduck <39456236+kboduck@users.noreply.github.com>
2023-01-12 09:59:55 +0200

    add reading check input

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 window.onload=function(){
+    var input = document.getElementById('source');
     const callback = () => {
-    alert("Hello, World!");
     console.log("Hello world");
+    if(input !== null){
+        console.log(input.value);
+    }
     };
-    const button = document.querySelector('#myButton');
-button.addEventListener('click', callback);
-var el = document.getElementById('result');
-
+    var el = document.getElementById('result');
 function someFunc(){
     el.value = 1231321;
+    console.log("someFunc");
 }
+    const button = document.querySelector('#myButton');
+button.addEventListener('click', callback);
+button.addEventListener('click', someFunc);
 }
