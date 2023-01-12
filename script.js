5bc65e3ff82e83a0ca11cc7b66b8a6cff556fd1b
kboduck <39456236+kboduck@users.noreply.github.com>
2023-01-12 17:41:11 +0200

    add buttons, style, clear input's.

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 window.onload=function(){
     var input = document.getElementById('source');
     const callback = () => {
-    console.log("Hello world");
-    if(input !== null){
-        console.log(input.value);
-    }
-    };
+    console.log(input.value);
+    var source = input.value;
+    var point = 0;
+    for(i = 0; i < source.length; i++){
+        if(isNaN(source[i])){
+            if(point == 2){
+                res += ";"
+                point = 0;
+                //continue;
+            }
+            if(source[i] == "."){
+                point = 2;
+            }
+            }
+            else{
+                res += source[i];
+                //point++;
+            }
+        }
+};
     var el = document.getElementById('result');
-function someFunc(){
-    el.value = 1231321;
-    console.log("someFunc");
-}
-    const button = document.querySelector('#myButton');
+    var res = "";
+        function someFunc(){
+            el.value = res;
+        }
+        function clear_result(){
+            el.value = "";
+            res = "";
+        }
+        function clear_task(){
+            input.value = "";
+        }
+const button = document.querySelector('#myButton');
+const clear = document.querySelector("#clear");
+const clear_input = document.querySelector('#clear_task')
 button.addEventListener('click', callback);
 button.addEventListener('click', someFunc);
+clear.addEventListener('click', clear_result);
+clear_input.addEventListener('click', clear_task);
 }
